5704d9f344794396322a70892f0f86052bae5b40
wells <qishan.wu.1@lexisnexis.com>
2017-10-13 18:09:15 +0800

    refs: 添加注释

--- server/server.php
+++ server/server.php
@@ -27,8 +27,16 @@ class WebSocketChat
         $this->server = new swoole_websocket_server(getenv('WEBSOCKET_SERVER'), getenv('WEBSOCKER_PORT'));
 
         $this->server->set(array(
-            'daemonize' => false,
-            'worker_num' => 1,
+            'daemonize' => false,       // 是否以守护进程方式运行
+            'worker_num' => 4,          // worker进程数，异步非阻塞为CPU的1-4倍即可，如果同步阻塞配置为100+，具体看处理请求负载
+            'reactor_num' => 2,         // 线程数量，默认设置为CPU核数
+            'max_conn' => 10000,        // 最大允许连接数
+            'max_request' => 2000,      // 表示进程处理完n次请求后结束运行，manager会自动重新创建一个worker进程，防止进程溢出。0表示不重启
+            'backlog' => 128,           // 决定同时有多少个待accept的连接，正常情况不会出现大量排队的情况
+            'open_cpu_affinity' => 1,  // 启用CPU亲和设置
+            'open_tcp_nodelay' => 1,   // 启用tcp_nodelay
+            'tcp_defer_accept' => 5,   // 设定一个秒数，当客户端连接连接到服务器时，在约定秒数内并不会触发accept，直到有数据发送，或者超时时才会触发
+            'log_file' => 'swoole.log',// 输出的日志文件，默认打印在屏幕上
         ));
 /*
         $this->server->on('Start', function (swoole_websocket_server $server) {
